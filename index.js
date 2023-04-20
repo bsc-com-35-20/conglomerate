92e2e0a670fd0d1573d3490ee9d5783705d0bccf
bsc-com-35-20 <bsc-com-35-20@unima.ac.mw>
2023-04-20 01:37:11 +0100

    Implemented 2 features

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 //importing 
 const express = require ('express')
 const app = express()
+const mysql = require('mysql')
+app.use(express.json());
 
-const post = require ('./src/posts/post.model')
 
-app.get('/api/v1', function(req,res) {
+const {posts} =require('./src/posts/post.model')
+const dbConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'chisomo',
+    password: '12345678',
+    database: 'conglomerate'
+});
 
-    return res.json(req)
+//feature 1
+//code to create a new user
+app.post('/users', (req,res)=>{
+    const First_name = req.body.First_name;
+    const Last_name = req.body.Last_name;
+    const Email_address= req.body.Email_address;
+    const phone_number = req.body.phone_number;
+    const sex = req.body.sex;
+
+    dbConnection.query('INSERT INTO users (First_name, Last_name, Email_address, phone_number, sex) values (?,?,?,?,?)', [First_name, Last_name, Email_address, phone_number, sex], (err,result)=>{
+        if (err){
+            console.log(err)
+        }else{
+            res.send("posted")
+        }
+    })
+
 })
-app.get('/api/posts', function(req,res) {
 
-    return res.json([post])
+//feature 2
+//get all users
+app.get('/users', (req,res)=>{
+    dbConnection.query('SELECT * FROM users', function(err,result){
+        if (err){
+            console.log(err)
+        } else {
+            res.send(result)
+        }
+    })
 })
+
+
 app.listen(3000,function(){
     console.log('Conglomerate listening on port 3000')
+
+    dbConnection.connect(function(err){
+        if (err) throw err;
+        console.log("connected to MySQL");
+    });
+    
 })
